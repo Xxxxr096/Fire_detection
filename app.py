83da28e2aee3cca0f0ff5c5905dcd7b5efcb1ffe
Amine Chabane <amine.chabane.edu@groupe-gema.com>
2025-02-17 14:49:26 +0100

    finision

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model  # type: ignore
 from PIL import Image
 
 # Charger le modèle de détection d'incendie
